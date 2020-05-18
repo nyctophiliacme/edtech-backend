30ab4b815b345201f35299d06ca2eb84a9823ce1
nyctophiliacme <pransh7575@gmail.com>
2020-05-18 20:32:27 +0530

    Added token authentication

--- superteacher/settings.py
+++ superteacher/settings.py
@@ -159,3 +159,9 @@ ACCOUNT_AUTHENTICATION_METHOD = "email"
 ACCOUNT_CONFIRM_EMAIL_ON_GET = True
 ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = reverse_lazy('account_confirm_complete')
 ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = reverse_lazy('account_confirm_complete')
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
