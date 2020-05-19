d893c7761aa6d5815d23368877748651b0bf8168
nyctophiliacme <pransh7575@gmail.com>
2020-05-19 19:56:00 +0530

    Removed password reset

--- emailsignup/urls.py
+++ emailsignup/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from allauth.account.views import ConfirmEmailView, PasswordResetFromKeyView
+from allauth.account.views import ConfirmEmailView
 from .views import complete_view, null_view
 
 urlpatterns = [
@@ -7,7 +7,6 @@ urlpatterns = [
     url(r'^registration/account-email-verification-sent/', null_view, name='account_email_verification_sent'),
     url(r'^registration/account-confirm-email/(?P<key>[-:\w]+)/$', ConfirmEmailView.as_view(), name='account_confirm_email'),
     url(r'^registration/complete/$', complete_view, name='account_confirm_complete'),
-    url(r'^password-reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', PasswordResetFromKeyView.as_view(), name='password_reset_confirm'),
     # Default urls
     url(r'^accounts/', include('allauth.urls')),
     url(r'', include('rest_auth.urls')),
