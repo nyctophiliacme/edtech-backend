12a63a880f9050e70f8d95b3463fa6a4da20ccdd
nyctophiliacme <pransh7575@gmail.com>
2020-05-19 23:27:52 +0530

    Updating the extra fields

--- emailsignup/serializers.py
+++ emailsignup/serializers.py
@@ -29,6 +29,12 @@ class CustomRegisterSerializer(RegisterSerializer):
             'date_of_birth': self.validated_data.get('date_of_birth', ''),
         }
 
+    def custom_signup(self, request, user):
+        user.phone_number = self.validated_data.get('phone_number', '')
+        user.city = self.validated_data.get('city', '')
+        user.date_of_birth = self.validated_data.get('date_of_birth', '')
+        user.save(update_fields=['phone_number', 'city', 'date_of_birth'])
+
 
 class UserSerializer(serializers.ModelSerializer):
 
