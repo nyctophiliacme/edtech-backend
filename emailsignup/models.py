f3a77c521375bf267c8ec1f5eab7902125481c13
nyctophiliacme <pransh7575@gmail.com>
2020-05-20 19:33:13 +0530

    Added extra fields in User Model

--- emailsignup/models.py
+++ emailsignup/models.py
@@ -11,9 +11,16 @@ class CustomUser(AbstractUser):
 
     objects = CustomUserManager()
 
-    phone_number = models.CharField(blank=True, max_length=100)
+    phone_number = models.CharField(blank=True, max_length=30)
+    current_class = models.CharField(blank=True, null=True, max_length=10)
+    school = models.CharField(blank=True, null=True, max_length=255)
+    city = models.CharField(blank=True, null=True, max_length=255)
     date_of_birth = models.DateField(blank=True, null=True)
-    city = models.DateField(blank=True, null=True)
+    is_paid_user = models.BooleanField(default=False)
+
+    parent_name = models.CharField(blank=True, null=True, max_length=255)
+    parent_email = models.CharField(blank=True, null=True, max_length=255)
+    parent_phone_number = models.CharField(blank=True, null=True, max_length=30)
 
     def __str__(self):
         return self.email
--- emailsignup/serializers.py
+++ emailsignup/serializers.py
@@ -9,11 +9,20 @@ class CustomRegisterSerializer(RegisterSerializer):
     password1 = serializers.CharField(write_only=True)
     password2 = serializers.CharField(write_only=True)
     email = serializers.EmailField(required=True)
+
     first_name = serializers.CharField(required=True)
     last_name = serializers.CharField(required=True)
     phone_number = serializers.CharField(required=True)
+
+    current_class = serializers.CharField(required=False)
+    school = serializers.CharField(required=False)
     city = serializers.CharField(required=False)
+
     date_of_birth = serializers.DateField(required=False)
+
+    parent_name = serializers.CharField(required=False)
+    parent_email = serializers.CharField(required=False)
+    parent_phone_number = serializers.CharField(required=False)
 
     def get_cleaned_data(self):
         super(CustomRegisterSerializer, self).get_cleaned_data()
@@ -25,21 +34,48 @@ class CustomRegisterSerializer(RegisterSerializer):
             'first_name': self.validated_data.get('first_name', ''),
             'last_name': self.validated_data.get('last_name', ''),
             'phone_number': self.validated_data.get('phone_number', ''),
+
+            'current_class': self.validated_data.get('current_class', ''),
+            'school': self.validated_data.get('school', ''),
             'city': self.validated_data.get('city', ''),
+
             'date_of_birth': self.validated_data.get('date_of_birth', ''),
+
+            'parent_name': self.validated_data.get('parent_name', ''),
+            'parent_email': self.validated_data.get('parent_email', ''),
+            'parent_phone_number': self.validated_data.get('parent_phone_number', ''),
         }
 
     def custom_signup(self, request, user):
-        if self.validated_data.get('phone_number', ''):
-            user.phone_number = self.validated_data.get('phone_number', '')
+        phone_number = self.validated_data.get('phone_number', '')
+        current_class = self.validated_data.get('', '')
+        school = self.validated_data.get('', '')
+        city = self.validated_data.get('', '')
 
-        if self.validated_data.get('city', ''):
-            user.city = self.validated_data.get('city', '')
+        date_of_birth = self.validated_data.get('', '')
+        parent_name = self.validated_data.get('', '')
+        parent_email = self.validated_data.get('', '')
+        parent_phone_number = self.validated_data.get('', '')
 
-        if self.validated_data.get('date_of_birth', ''):
-            user.date_of_birth = self.validated_data.get('date_of_birth', '')
+        if phone_number:
+            user.phone_number = phone_number
+        if current_class:
+            user.current_class = current_class
+        if school:
+            user.school = school
+        if city:
+            user.city = city
+        if date_of_birth:
+            user.date_of_birth = date_of_birth
+        if parent_name:
+            user.parent_name = parent_name
+        if parent_email:
+            user.parent_email = parent_email
+        if parent_phone_number:
+            user.parent_phone_number = parent_phone_number
 
-        user.save(update_fields=['phone_number', 'city', 'date_of_birth'])
+        user.save(update_fields=['phone_number', 'current_class', 'school', 'city', 'date_of_birth', 'parent_name',
+                                 'parent_email', 'parent_phone_number'])
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -47,5 +83,3 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['pk', 'email', 'first_name', 'last_name', 'phone_number', 'date_of_birth', 'city']
-
-
