984e52c365d25177991fc30fd649aa97a4465eaf
nyctophiliacme <pransh7575@gmail.com>
2020-05-24 03:58:44 +0530

    Modified class based view

--- exams/views.py
+++ exams/views.py
@@ -1,23 +1,20 @@
-from django.views import View
 from exams.models import Exam
 from exams.serializers import ExamSerializer
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
-class ExamView(View):
+class ExamView(APIView):
 
-    @api_view(['GET'])
     def get(self, request, *args, **kwargs):
-        if request.method == 'GET':
-            exams = Exam.objects.all()
-            serializer = ExamSerializer(exams, many=True)
-            return Response(serializer.data)
+        exams = Exam.objects.all()
+        serializer = ExamSerializer(exams, many=True)
+        return Response(serializer.data)
 
-    @api_view(['POST'])
     def post(self, request, *args, **kwargs):
         serializer = ExamSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
