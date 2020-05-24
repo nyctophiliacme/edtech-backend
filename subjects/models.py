e4b0c5e1862c710c2890b3862b45c30da0c4d385
nyctophiliacme <pransh7575@gmail.com>
2020-05-24 20:24:24 +0530

    Triggering cascade delete when the exam object is deleted

--- subjects/models.py
+++ subjects/models.py
@@ -7,7 +7,7 @@ class Subject(models.Model):
     description = models.TextField(blank=True, null=True)
     image_url = models.TextField(blank=True, null=True)
 
-    exam = models.ForeignKey('exams.Exam')
+    exam = models.ForeignKey('exams.Exam', on_delete=models.CASCADE)
 
     created_on = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
