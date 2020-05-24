f7d6ee63675526b1ddf538d71eaf28db8d93a750
nyctophiliacme <pransh7575@gmail.com>
2020-05-24 23:42:20 +0530

    Resolving bug in fetching Chapters

--- chapters/serializers.py
+++ chapters/serializers.py
@@ -5,7 +5,7 @@ from chapters.models import Chapter
 
 class ChapterSerializer(serializers.ModelSerializer):
     subject_code = serializers.CharField(source='subject.subject_code', read_only=True)
-    exam_code = serializers.CharField(source='exam.code', read_only=True)
+    exam_code = serializers.CharField(source='exam.exam_code', read_only=True)
 
     class Meta:
         model = Chapter
