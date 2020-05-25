a25164b10f3098efdc782b5130ef8af1fbb7b625
nyctophiliacme <pransh7575@gmail.com>
2020-05-25 21:13:00 +0530

    Resolved bug of Querying

--- questions/views.py
+++ questions/views.py
@@ -10,7 +10,7 @@ class QuestionViewChapterVise(APIView):
 
     def get(self, request, *args, **kwargs):
         chapter_id = request.query_params.get('chapter_id')
-        question_ids = QuestionChapterMapping.objects.filter(chapter_id=chapter_id).values_list('pk', flat=True)
+        question_ids = QuestionChapterMapping.objects.filter(chapter_id=chapter_id).values_list('question_id', flat=True)
 
         questions = Question.objects.filter(id__in=question_ids)
         serializer = QuestionSerializer(questions, many=True)
