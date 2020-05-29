296050372eb9cab32ffa55ef80e65ca00072f824
nyctophiliacme <pransh7575@gmail.com>
2020-05-29 21:37:54 +0530

    Script added to modify the question count

---
+++ questions/scripts/update_question_count_of_chapters.py
@@ -0,0 +1,23 @@
+import django
+import os
+import sys
+
+sys.path.append("/home/ubuntu/edtech-backend")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'superteacher.settings')
+django.setup()
+
+from exams.models import Exam
+from subjects.models import Subject
+from chapters.models import Chapter
+from questions.models import QuestionChapterMapping
+
+if __name__ == '__main__':
+    exam = Exam.objects.get(exam_code=sys.argv[1])
+    subjects = Subject.objects.filter(exam=exam)
+
+    for subject in subjects:
+        chapters = Chapter.objects.filter(subject=subject)
+        for chapter in chapters:
+            question_count = 0
+            chapter.question_count = QuestionChapterMapping.objects.filter(chapter=chapter).count()
+            chapter.save()
