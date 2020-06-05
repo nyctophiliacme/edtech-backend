8c9d09601b7c9bbc2c159db93b15754cc3d25af8
nyctophiliacme <pransh7575@gmail.com>
2020-06-05 19:24:42 +0530

    Modified API to fetch using subject and exam code

--- user_question/views.py
+++ user_question/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from user_question.models import UserQuestionProgress
+from exams.models import Exam
+from subjects.models import Subject
 
 
 class UserQuestionProgressView(APIView):
@@ -12,14 +14,18 @@ class UserQuestionProgressView(APIView):
 
     def post(self, request, *args, **kwargs):
         user_id = request.user.id
+        exam_obj = Exam.objects.get(exam_code=request.data.get('exam_code'))
+        subject_obj = Subject.objects.get(exam=exam_obj, subject_code=request.data.get('subject_code'))
+
         try:
             user_question_progress = UserQuestionProgress.objects.get(user_id=user_id,
                                                                       question_id=request.data.get('question'))
             serializer = UserQuestionProgressSerializer(user_question_progress, data=request.data)
+
         except UserQuestionProgress.DoesNotExist:
             serializer = UserQuestionProgressSerializer(data=request.data)
 
         if serializer.is_valid():
-            serializer.save(user_id=user_id)
+            serializer.save(user_id=user_id, exam=exam_obj, subject=subject_obj)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
