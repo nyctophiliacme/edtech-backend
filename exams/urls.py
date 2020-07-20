c95a98c5c4c5271996838692556618d5a712a7c7
nyctophiliacme <pransh7575@gmail.com>
2020-07-20 15:59:16 +0530

    Added Course ID based Exam view

--- exams/urls.py
+++ exams/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from exams.views import ExamView
+from exams.views import ExamView, ExamCourseIdViseView
 
 urlpatterns = [
     path('', ExamView.as_view()),
+    path('course_vise', ExamCourseIdViseView.as_view())
 ]
--- exams/views.py
+++ exams/views.py
@@ -18,3 +18,11 @@ class ExamView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ExamCourseIdViseView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        exams = Exam.objects.filter(course_id=request.query_params.get("course_id"))
+        serializer = ExamSerializer(exams, many=True)
+        return Response(serializer.data)
