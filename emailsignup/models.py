e19069e0b4a6a82edf3844c397dfb7a72318854a
nyctophiliacme <pransh7575@gmail.com>
2020-07-29 21:47:58 +0530

    Saving username as email in social login

--- emailsignup/models.py
+++ emailsignup/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 from emailsignup.managers import CustomUserManager
 
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
+
 
 class CustomUser(AbstractUser):
 
@@ -24,3 +27,8 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+@receiver(pre_save, sender=CustomUser)
+def update_username_from_email(sender, instance, **kwargs):
+    instance.username = instance.email
