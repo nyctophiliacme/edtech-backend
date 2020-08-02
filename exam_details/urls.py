ddcdecada2038368e3b11c55332836a485d671a8
nyctophiliacme <pransh7575@gmail.com>
2020-08-03 01:42:55 +0530

    Modified urls for exam details post

--- exam_details/urls.py
+++ exam_details/urls.py
@@ -4,7 +4,7 @@ from exam_details.views import ExamSectionContainerExamViseView, ExamSectionCont
 
 urlpatterns = [
     path('section_container/', ExamSectionContainerView.as_view()),
-    path('section_container/exam_vise/', ExamSectionContainerExamViseView.as_view()),
+    path('section_container_exam_vise/', ExamSectionContainerExamViseView.as_view()),
     path('section_details/', ExamSectionDetailsContainerViseView.as_view()),
-    path('section_details/container_vise/', ExamSectionDetailsView.as_view()),
+    path('section_details_container_vise/', ExamSectionDetailsView.as_view()),
 ]
