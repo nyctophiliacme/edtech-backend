43fd593b1c75bb11035550b45eda87fccca36b5f
nyctophiliacme <pransh7575@gmail.com>
2020-08-03 01:36:18 +0530

    Modified Get urls for exam details apis

--- exam_details/urls.py
+++ exam_details/urls.py
@@ -3,8 +3,8 @@ from exam_details.views import ExamSectionContainerExamViseView, ExamSectionCont
     ExamSectionDetailsContainerViseView, ExamSectionDetailsView
 
 urlpatterns = [
-    path('section_container', ExamSectionContainerView.as_view()),
+    path('section_container/', ExamSectionContainerView.as_view()),
     path('section_container/exam_vise/', ExamSectionContainerExamViseView.as_view()),
-    path('section_details', ExamSectionDetailsContainerViseView.as_view()),
+    path('section_details/', ExamSectionDetailsContainerViseView.as_view()),
     path('section_details/container_vise/', ExamSectionDetailsView.as_view()),
 ]
