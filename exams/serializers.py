8d802c7cdcaf45ac39b2e9aa22def2f6e7c08476
nyctophiliacme <pransh7575@gmail.com>
2020-08-15 15:51:10 +0530

    Added course title in exam get API

--- exams/serializers.py
+++ exams/serializers.py
@@ -4,6 +4,8 @@ from exams.models import Exam
 
 
 class ExamSerializer(serializers.ModelSerializer):
+    course_title = serializers.CharField(source='course.course_title', read_only=True)
+
     class Meta:
         model = Exam
-        fields = ('id', 'exam_code', 'title', 'description', 'image_url', 'course')
+        fields = ('id', 'exam_code', 'title', 'description', 'image_url', 'course', 'course_title')
