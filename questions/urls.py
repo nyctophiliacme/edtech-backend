7d77f74644c792253ef03035c05705100885b93c
nyctophiliacme <pransh7575@gmail.com>
2020-08-30 17:11:42 +0530

    Exposed the question bug report url

--- questions/urls.py
+++ questions/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from questions.views import QuestionViewChapterVise, QuestionPostView
+from questions.views import QuestionViewChapterVise, QuestionPostView, QuestionBugReportView
 
 urlpatterns = [
     path('practice/', QuestionViewChapterVise.as_view()),
-    path('save_data/', QuestionPostView.as_view())
+    path('save_data/', QuestionPostView.as_view()),
+    path('bug_report/', QuestionBugReportView.as_view()),
 ]
