03cffd35e12b164f846e8c696072fe7bf8141402
nyctophiliacme <pransh7575@gmail.com>
2020-09-13 01:46:08 +0530

    Specifying the from email explictly

--- commons/custom_email.py
+++ commons/custom_email.py
@@ -1,4 +1,5 @@
 from django.core.mail import send_mail
+from superteacher.settings import EMAIL_HOST_USER
 
 
 def send_help_request_email(user_name, is_guest_user, user_email, message):
@@ -9,6 +10,7 @@ def send_help_request_email(user_name, is_guest_user, user_email, message):
     send_mail(
         subject='User Reported an Issue in Superteacher',
         message=message,
+        from_email=EMAIL_HOST_USER,
         recipient_list=['Studykitco@gmail.com', 'info@superteacher.pk'],
         fail_silently=False,
     )
